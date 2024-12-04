c41fe5de53bf43579ab7ef4553a4266d4f155233
@sarahkolesnikova <kolesnisarah@gmail.com>
2024-12-04 16:17:27 -0300

    dockerfile

--- Sprint-4/Exercicios/ex05.py
+++ Sprint-4/Exercicios/ex05.py
@@ -1,20 +1,18 @@
 import csv
 
-def exportar_grade(file_path):
+def exportar_grades(file_path):
     with open(file_path, newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        students = []
+        arquivo = csv.reader(csvfile)
+        estudantes = []
         
-        for row in reader:
-            name = row[0]
+        for row in arquivo:
+            nome = row[0]
             grades = sorted(map(int, row[1:]), reverse=True)[:3]
-            average = round(sum(grades) / 3, 2)
-            students.append((name, grades, average))
+            media = round(sum(grades) / 3, 2)
+            estudantes.append((nome, grades, media))
         
-        students_sorted = sorted(students, key=lambda x: x[0])
+        estudantes_sorted = sorted(estudantes, key=lambda x: x[0])
         
-        for student in students_sorted:
-            print(f"Nome: {student[0]} Notas: {student[1]} Média: {student[2]}")
-
-# Exemplo de chamada da função
-exportar_grade('/c:/Users/Usuario/Desktop/compassacademy/Sprint-4/Exercicios/estudantes.csv')
+        for estudante in estudantes_sorted:
+            print(f"Nome: { estudante[0]} Notas: { estudante[1]} Média: { estudante[2]}")
+exportar_grades ("estudantes.csv")
