0201a4f5d41d2040e61f70bdd9a01cc9d8c7d78d
@sarahkolesnikova <kolesnisarah@gmail.com>
2025-01-06 23:00:08 -0300

    desafio

--- Sprint-6/Desafio/app/upar_csvs.py
+++ Sprint-6/Desafio/app/upar_csvs.py
@@ -2,10 +2,10 @@ import boto3 #importando a biblioteca boto3
 import datetime #importando a biblioteca datetime
 import os #importando a biblioteca os
 
-aws_access_key_id= 'ASIAQZFG4XPWN2TZ2YPK' #chave de acesso
-aws_secret_access_key = 'mFoUFdfsZzzHY9kydxqSjImRehSZdpVytOvPx8fe' #chave secreta
+aws_access_key_id= 'access_key' #chave de acesso
+aws_secret_access_key = 'secret_access' #chave secreta
 #token de sessão
-session_token = 'IQoJb3JpZ2luX2VjEF4aCXVzLWVhc3QtMSJHMEUCIQDdgh3KN7q43nmaC6/3ji31NQYSme5C25EHVRGO6gs7RQIgT6IAAVeB6KJ2gycf5XM084IsK49P4EU5Eb6xzLumuA4qpwMIRhAAGgwwNTQwMzcxMDE1NDgiDDTa3Jm+wb5RYNY7+yqEAyAwzl/XwylMmEqW6S1nu/gMITm4rj8nZfhwdVRvn0MRABSuXFQ5XEncekn3EuBbB9vWK+PECbma3zYlxe8VbWy0k7VWe6BuhhjSSaMUmt77nO9RSWs3buDAeG3GqbMNLaG6qCG6NxZOYbLz7SXw1fE9ftA/tI5L4UvRCE+ECXxkraLZqAeWY4WI5jJ59pe9Q4cv53qXzrpPLngCW/+2HCag12dNgXvcmn4jUMiByBjukCBNLQxO3RH/JwBTKyegrQ2PkCZyRAUid6wcEkp1HfCxghGib1o4KQvuzsBaYNjJ5n+S93Bmb7S4rit92sirTxxrUoB0/snfWpQRW/JMrStur8V5Ft6DxHCe3gWdwkbp7uTd9mKvQl1xw9Lb8RtTxJCXxYRnsQ9mCwWkkoKEWA9ab8zbQFgnlIwQfreA7LXKlcTeVrsJwdHHEqv6nic0Vf+VtH1JLTtiS/kQw2dBDvxEfktnnOEKLIfTtT8SFzCtSXC4DVeJs4Oz+xAPmZRBoz0MvNkw6KzvuwY6pgH/+JfnYfxEuik7oSnlMWOMPtENqbMOF8JwP+ZXVVgbY7gcMHHOzc8Y7WK7bgt4mhvPdI5RE3aEg7Md4jyqIxC/eImHRYN9f6Uy6zNK/ax+V1Tq9krxxMIWeq6RzHyjbDhgn8KRpWxotIpnnpoogvOP/rdwdmUGlc0cSmXixlq8APF/fyjBdGDyqZDqIR6mZaIZnaMJxvc9AcIxJfEQJZtSJVgVXzZB'
+session_token = 'token'
 bucket_name = 'desafio-final-sarah' # nome do bucket
 file_path1 = 'C:/Users/Usuario/Desktop/compassacademy/Sprint-6/Desafio/movies.csv' # caminho do arquivo movies.csv
 file_path2 = 'C:/Users/Usuario/Desktop/compassacademy/Sprint-6/Desafio/series.csv' # caminho do arquivo series.csv
